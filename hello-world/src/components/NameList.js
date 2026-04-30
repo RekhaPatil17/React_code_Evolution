96102529bfdbd54f753ddba66105f76c682648f1
RekhaNPatil <rekhapatil9243@gmail.com>
2026-04-30 22:10:51 +0530

    list keys

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -24,7 +24,7 @@ function NameList() {
     ]
     const personList = persons.map(person => 
     (
-        <Person person={person}></Person>
+        <Person key={person.name} person={person}></Person>
 
     )
     )
