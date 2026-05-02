e6c857d729a4e56d7d51b4d607ec5e647ef65617
RekhaNPatil <rekhapatil9243@gmail.com>
2026-05-02 22:49:57 +0530

    Refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -22,12 +22,17 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
 
-      <ParentComp />
+
+
+      <RefsDemo />
+      
+      {/* <ParentComp /> */}
       
           {/* <PureComp /> */}
         {/* <Table /> */}
