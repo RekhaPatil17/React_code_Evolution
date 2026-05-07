f46ae1bae7685fb8e266b5858f8a3523cc786f79
RekhaNPatil <rekhapatil9243@gmail.com>
2026-05-07 21:40:41 +0530

    Context part 2

--- hello-world/src/ComponentE.js
+++ hello-world/src/ComponentE.js
@@ -1,13 +1,23 @@
 import React,{Component} from 'react';
 import ComponentF from './ComponentF';  
+import UserContext from './components/userContext';
 class ComponentE extends Component {
+    static contextType = UserContext
+
   render() {
-    return (    
-      <ComponentF / >
+    return (  
+        
+        <div>
+            Component E context value {this.context}
+
+                  <ComponentF / >
+
+        </div>
     );
   }
 }
 
+{/* ComponentE.contextType = UserContext */}
 export default ComponentE; 
 
 
